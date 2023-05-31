dfc4e1ab54f971be456d46a6576ee00337aa47ef
elegance-event <134351336+elegance-event@users.noreply.github.com>
2023-05-31 15:43:59 +0530

    Add files via upload

---
+++ js/java.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    // Counterup
+    $('.counter').counterUp({
+      time: 1000
+    });
+  
+    // Main Navigation
+    $('#menu-toggle').click(function() {
+      $(this).toggleClass('open');
+      $('.main-nav').toggleClass('show-it');
+    });
+  });
